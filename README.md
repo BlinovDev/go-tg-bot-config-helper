960062df750bdeb0d125d5b6ab8fd13e9a039dd3
Anton <anton253647@gmail.com>
2024-10-27 15:53:20 +0100

    Refactored possible fields

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # go-tg-bot-config-helper
+
+## Description
+
+Its a simple config helper that can be really handful to use if you start Telegram bots development with Go.
+It can inspire you to create some functionality based on fields desribed in config.
+
+## How to use?
+
+Import the lib in your Go project:
+```Go
+tgconfighelper "github.com/BlinovDev/go-tg-bot-config-helper"
+```
+
+And call LoadConfig method:
+```Go
+config, err := tgconfighelper.LoadConfig()
+if err != nil {
+	log.Fatalf("Failed to load config: %v", err)
+}
+
+bot, err := tgbotapi.NewBotAPI(config.Bot.Token)
+if err != nil {
+	log.Panic(err)
+}
+```
+
+## Config example
+
--- tgconfighelper.go
+++ tgconfighelper.go
@@ -5,17 +5,28 @@ import (
 	"io/ioutil"
 )
 
-// define struct to access config file
+// define struct to store config
 type Config struct {
 	Bot struct {
-		Token           string `yaml:"token"`
-		WebHookURL      string `yaml:"webhook_url"`
-		CertFile        string `yaml:"cert_file"`
-		KeyFile         string `yaml:"key_file"`
-		CronTimePattern string `yaml:"cron_time_pattern"`
+		// For any telegram bot
+		Token string `yaml:"token"`
+		// For GetUpdates approach
+		Timeout int `yaml:"timeout"`
+		// For Webhook approach
+		WebHookURL string `yaml:"webhook_url"`
+		CertFile   string `yaml:"cert_file"`
+		KeyFile    string `yaml:"key_file"`
+		// Environment(can be set here to prevent its passing through ENV var)
+		Environment string `yaml:"environment"`
+		// Some reserved configs for cron setup
+		FirstCron  string `yaml:"first_cron_time_pattern"`
+		SecondCron string `yaml:"second_cron_time_pattern"`
+		// Path to your storage file if you don't use DB
+		FilePath string `yaml:"file_path"`
 	} `yaml:"bot"`
 }
 
+// Reads config.yml in project root and returns config if file was parsed to Config successfully
 func LoadConfig() (*Config, error) {
 	var config Config
 
